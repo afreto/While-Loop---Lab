fe6d5b43d190815d02d7ad7f90315b281112dd59
Момчил Миланов <deadman_kula@abv.bg>
2021-02-01 19:32:45 +0200

    solved

--- 05. Account Balance/Program.cs
+++ 05. Account Balance/Program.cs
@@ -6,7 +6,29 @@ namespace _05._Account_Balance
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double totalMoney = 0;
+            double sum = 0;
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "NoMoreMoney")
+                {
+                    Console.WriteLine($"Total: {totalMoney:f2}");
+                    break;
+                }
+                else
+                {
+                    sum = double.Parse(input);
+                    if (sum < 0)
+                    {
+                        Console.WriteLine("Invalid operation!");
+                        Console.WriteLine($"Total: {totalMoney:f2}");
+                        break;
+                    }
+                    totalMoney += sum;
+                }
+                Console.WriteLine($"Increase: {sum:f2}");
+            }
         }
     }
 }
