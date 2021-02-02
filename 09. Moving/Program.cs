0c1689715e3305b7873d3b7c192df6929143f6cf
Момчил Миланов <deadman_kula@abv.bg>
2021-02-03 00:27:29 +0200

    solved

--- 09. Moving/Program.cs
+++ 09. Moving/Program.cs
@@ -6,7 +6,29 @@ namespace _09._Moving
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int width = int.Parse(Console.ReadLine());
+            int length = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+            int area = width * length * height;
+            int boxes = 0;
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input != "Done")
+                {
+                    boxes += int.Parse(input);
+                    if (boxes > area)
+                    {
+                        Console.WriteLine($"No more free space! You need {boxes - area} Cubic meters more.");
+                        break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"{area - boxes} Cubic meters left.");
+                    break;
+                }
+            }
         }
     }
 }
