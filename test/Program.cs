aa5c0e2022faf34ff9647bfd54a3ce923c8da923
Момчил Миланов <deadman_kula@abv.bg>
2021-02-06 11:40:39 +0200

    q

---
+++ test/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num = double.Parse(Console.ReadLine());
+            double sum = num * 100;
+            Console.WriteLine(sum);
+        }
+    }
+}
